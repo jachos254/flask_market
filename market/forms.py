a0f02fc6dca67b9853947440637a4d78b3477368
Jachos <jachowski.m@gmail.com>
2021-07-12 21:33:00 +0200

    validators not working on another pc

--- market/forms.py
+++ market/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Length, EqualTo
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Username:', validators=Length(min=2, max=30))
+    username = StringField(label='Username:')
     email_address = StringField(label='E-mail:')
-    password1 = PasswordField(label='Password:', validators=Length(min=6))
+    password1 = PasswordField(label='Password:')
     password2 = PasswordField(label='Repeat Password:')
     submit = SubmitField(label='Create Account')
